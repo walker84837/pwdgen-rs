13820c7ad570456d32a1db08be613741fea346d1
#84837 <walker84837@gmail.com>
2023-11-27 22:45:38 +0100

    Refactor build settings, dependencies, bump versions and update files (#1)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,23 +5,40 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/),
 and this project adheres to [Semantic Versioning](https://semver.org/).
 
-## [Unreleased]
+## \[Unreleased\]
 
-## [0.1.0] - 2023-11-02
+## \[0.2.0\] - 2023-11-27
 
 ### Added
 
-- Initial release of `cpasswordgenerator`.
+  - None.
 
 ### Fixed
 
-- None.
+  - None.
 
 ### Changed
 
-- None.
+  - Replaced C's `rand()` function with OpenSSL's random function for security purposes.
 
 ### Removed
 
-- None.
+  - None.
 
+## \[0.1.0\] - 2023-11-02
+
+### Added
+
+  - Initial release of `cpasswordgenerator`.
+
+### Fixed
+
+  - None.
+
+### Changed
+
+  - None.
+
+### Removed
+
+  - None.
--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <time.h>
 #include <argp.h>
+#include <openssl/rand.h>
 
 void clamp(int *value, int min, int max)
 {
@@ -15,18 +16,35 @@ void clamp(int *value, int min, int max)
 	}
 }
 
-char* generate_password()
+int get_random_int(void)
+{
+	unsigned char buffer[4];
+	if (RAND_bytes(buffer, sizeof(buffer)) != 1) {
+		perror("Error generating random bytes");
+		exit(EXIT_FAILURE);
+	}
+
+	int randomInteger;
+	memcpy(&randomInteger, buffer, sizeof(randomInteger));
+
+	if (randomInteger < 0) {
+		randomInteger *= -1;
+	}
+
+	return randomInteger;
+}
+
+char *generate_password()
 {
 	char lowercase_letters[] = "abcdefghijklmnopqrstuvwxyz";
-	char uppercase_letter = (char)('A' + rand() % 26);
-	char number = (char)('0' + rand() % 10);
+	char uppercase_letter = (char)('A' + get_random_int() % 26);
+	char number = (char)('0' + get_random_int() % 10);
 	char letters[19];
 
 	int i = 0;
 
-	/* Shuffle lowercase_letters using Fisher-Yates shuffle */
 	for (i = 25; i > 0; i--) {
-		int j = rand() % (i + 1);
+		int j = get_random_int() % (i + 1);
 		char temp = lowercase_letters[i];
 		lowercase_letters[i] = lowercase_letters[j];
 		lowercase_letters[j] = temp;
@@ -40,16 +58,18 @@ char* generate_password()
 	}
 
 	/* Replace one character in each group with uppercase and number */
-	letters[rand() % 6] = uppercase_letter;
-	letters[6 + rand() % 6] = uppercase_letter;
-	letters[12 + rand() % 6] = number;
+	letters[get_random_int() % 6] = uppercase_letter;
+	letters[6 + get_random_int() % 6] = uppercase_letter;
+	letters[12 + get_random_int() % 6] = number;
 	letters[18] = '\0';
 
 	/* Allocate 22 bytes of memory for the password */
 	char *password = (char *)malloc(22);
 	if (password != NULL) {
 		snprintf(password, 22, "%.*s-%.*s-%.*s", 6, letters, 6, letters + 6, 6, letters + 12);
-	} else {
+	}
+	
+	else {
 		perror("Error while generating the password");
 		exit(EXIT_FAILURE);
 	}
@@ -57,65 +77,64 @@ char* generate_password()
 	return password;
 }
 
-const char *argp_program_version = "C Password Generator 0.1.0";
+const char *argp_program_version = "C Password Generator 0.2.0";
 const char *argp_program_bug_address = "<walker84837@gmail.com>";
 
-static char doc[] = "Generate random passwords";
+static char doc[] = "Generate GetRandomIntom passwords";
 static char args_doc[] = "[OPTION]...";
 
 static struct argp_option options[] = {
-    {"num_passwords", 'n', "NUM_PASSWORDS", 0, "Number of passwords to generate", 0},
-    {0}
+	{"num_passwords", 'n', "NUM_PASSWORDS", 0, "Number of passwords to generate", 0},
+	{0}
 };
 
 struct arguments
 {
-    int num_passwords;
+	int num_passwords;
 };
 
 static error_t parse_opt(int key, char *arg, struct argp_state *state)
 {
-    struct arguments *arguments = (struct arguments *)state->input;
+	struct arguments *arguments = (struct arguments *)state->input;
 
-    switch (key) {
-        case 'n':
-            arguments->num_passwords = atoi(arg);
-            if (arguments->num_passwords <= 0) {
-                fprintf(stderr, "Invalid number of passwords. Setting password number to 1...\n");
-                arguments->num_passwords = 1;
-            }
-            break;
-        case ARGP_KEY_ARG:
-            break;
-        case ARGP_KEY_END:
-            clamp(&arguments->num_passwords, 1, 2048);
-            break;
-        default:
-            return ARGP_ERR_UNKNOWN;
-    }
-    return 0;
+	switch (key) {
+		case 'n':
+			arguments->num_passwords = atoi(arg);
+			if (arguments->num_passwords <= 0) {
+				fprintf(stderr, "Invalid number of passwords. Setting password number to 1...\n");
+				arguments->num_passwords = 1;
+			}
+			break;
+		case ARGP_KEY_ARG:
+			break;
+		case ARGP_KEY_END:
+			clamp(&arguments->num_passwords, 1, 2048);
+			break;
+		default:
+			return ARGP_ERR_UNKNOWN;
+	}
+	return 0;
 }
 
 static struct argp argp = {options, parse_opt, args_doc, doc};
 
 int main(int argc, char *argv[]) {
-    srand((unsigned int)time(NULL));
-    struct arguments args;
+	struct arguments args;
 
-    args.num_passwords = 1; // Default value
+	args.num_passwords = 1;
 
-    argp_parse(&argp, argc, argv, 0, 0, &args);
+	argp_parse(&argp, argc, argv, 0, 0, &args);
 
-    for (int i = 0; i < args.num_passwords; i++) {
-        char *password = generate_password();
-        if (password != NULL) {
-            puts(password);
-            free(password);
-        } else {
-            fputs("Failed to allocate memory for password.\n", stderr);
-            return 1;
-        }
-    }
+	for (int i = 0; i < args.num_passwords; i++) {
+		char *password = generate_password();
+		if (password != NULL) {
+			puts(password);
+			free(password);
+		} else {
+			fputs("Failed to allocate memory for password.\n", stderr);
+			return 1;
+		}
+	}
 
-    return 0;
+	return 0;
 }
